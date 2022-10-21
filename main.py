9e128d8be7c1d4985e0fadcdd4daf1c76af4d4a2
archie <archiebeales@gmail.com>
2022-10-21 21:36:25 +0200

    finished the validator part of the project

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 # main python file containing the files
-
-from concurrent.futures.process import _threads_wakeups
-
-from pyparsing import col
+from operator import index
 
 
 class sudokuSolver:
@@ -10,11 +7,11 @@ class sudokuSolver:
     class used to contain the methods that will be used to solve and initialise the sudoku
     using a class to make it more compatible later
     '''
-    def __init__(self):
+    def __init__(self,board):
         '''
         initialisor function
         '''
-        self.board = [] # the main sudo board is an array of 9 sub arrays, 
+        self.board = board # the main sudo board is an array of 9 sub arrays, 
                         # all containing 9 items, an item is either a number, 
                         # or a "x" meaning a number to solve
         
@@ -36,8 +33,8 @@ class sudokuSolver:
 
     def correct_column(self, column:int) -> bool:
         '''
-        input -> self, column:int
-        output -> boolean
+        input -> self, column:int 
+        output -> boolean 
         desc -> takes the integer as the index of the column 
                 and returns if the column has duplicates of a number.
         '''
@@ -45,7 +42,7 @@ class sudokuSolver:
         # making the list that represents the column
 
         # aha the funny
-        column_array =[self.board[x][column] for x in self.board]
+        column_array =[self.board[x][column] for x in range(len(self.board))]
         for item in column_array:
             if column_array.count(item) > 1 and item != "x":
                 return False
@@ -53,4 +50,62 @@ class sudokuSolver:
         #final return statement
         return True
 
-    #TODO make the function that checks the area
+
+    def correct_area(self, area:int) -> bool:
+        '''
+        input -> area: int
+        output -> boolean
+        desc -> takes an int, 
+                and checks to see if the ares described by the int
+                has any duplicate numbers, if not then true else false
+        '''
+        # array that contains the indexs for the column/row to take
+        indexs = [[0,1,2], [3,4,5], [6,7,8],[0,1,2], [3,4,5], [6,7,8],[0,1,2], [3,4,5], [6,7,8]]
+        res = [] # array that will contain all the numbers in the area
+        area_index = area
+        for i in range(len(self.board)):
+            for j in range(len(self.board[i])):
+                if i in indexs[area_index] and j in indexs[area_index]:
+                    res.append(self.board[i][j])
+        
+        # res now contains every number in the area we have to check
+        for item in res:
+            if res.count(item) > 1 and item != "x":
+                return False
+        
+        return True # final return statement
+
+    def all_correct(self):
+        '''
+        input -> none
+        output -> boolean
+        desc -> checks the entire sudoku to see if it breaks any of the functions
+            that we previously made
+        '''
+        # testing each column
+        for j in range(len(self.board[0])): 
+            if self.correct_column(j) != True:
+                return (False, "failed on column {}".format(j))
+        
+        #testing each row
+        for i in range(len(self.board)):
+            if self.correct_row(i) != True:
+                return (False, "failed on row {}".format(i))
+        
+        #testing each area
+        for k in range(0,9):
+            if self.correct_area(k) != True:
+                return (False, "failed on area {}".format(k))
+        return (True, "Verified all tests")
+
+board = [[1,2,'x','x','x','x','x','x',5],
+         ['x','x',2,'x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],
+         ['x','x','x','x','x','x','x','x','x'],]
+sudoku = sudokuSolver(board=board)
+print(sudoku.all_correct())
